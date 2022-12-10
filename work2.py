0cfa58966ba0019d30bda4e188e9a96cf5f6e2df
romanc <roman.zin1993@gmail.com>
2022-12-10 15:50:22 +0300

    work2

--- work2.py
+++ work2.py
@@ -33,4 +33,14 @@
 #     print(sum, end=" ")
 #     step += 1
 
+# Задайте список из n чисел последовательности (1+ 1/n)^n и выведите на экран их сумму.
+
+n = int(input('Введите число: '))
+step = 1
+sum = 0
+
+while step < n+1:
+    sum = sum + (1 + 1/step)**step
+    step += 1
+print(round(sum, 2))
 
