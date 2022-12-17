12d39f7b6830013a50d080fa489c8323ccec0a03
romanc <roman.zin1993@gmail.com>
2022-12-17 20:13:34 +0300

    comment

--- Work4.py
+++ Work4.py
@@ -73,29 +73,29 @@
 
 # Пример:
 
-# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+# # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-k = int(input('Введите число k: '))
+# k = int(input('Введите число k: '))
 
-def Fibonacci(n):
-    if n in [1, 2]:                       
-        return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+# def Fibonacci(n):
+#     if n in [1, 2]:                       
+#         return 1
+#     else:
+#         return Fibonacci(n-1) + Fibonacci(n-2)
 
-def NegaFibonacci(n):
-    if n == 1:                       
-        return 1
-    elif n == 2:                       
-        return -1
-    else:
-        num1, num2 = 1, -1
-        for i in range(2, n):
-            num1, num2 = num2, num1 - num2
-        return num2
+# def NegaFibonacci(n):
+#     if n == 1:                       
+#         return 1
+#     elif n == 2:                       
+#         return -1
+#     else:
+#         num1, num2 = 1, -1
+#         for i in range(2, n):
+#             num1, num2 = num2, num1 - num2
+#         return num2
 
-list = [0]
-for e in range(1, k + 1):
-    list.append(Fibonacci(e))
-    list.insert(0, NegaFibonacci(e))
-print(list)
+# list = [0]
+# for e in range(1, k + 1):
+#     list.append(Fibonacci(e))
+#     list.insert(0, NegaFibonacci(e))
+# print(list)
