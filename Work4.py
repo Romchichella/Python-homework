fd4ff0477fbc418e540589a87774bc52c6265426
romanc <roman.zin1993@gmail.com>
2022-12-17 20:12:47 +0300

    comment

--- Work4.py
+++ Work4.py
@@ -53,18 +53,49 @@
 
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
+# # Пример:
+
+# # - 45 -> 101101
+# # - 3 -> 11
+# # - 2 -> 10
+
+# a = int(input('Введите число: '))
+ 
+# b = ''
+ 
+# while a > 0:
+#     b = str(a % 2) + b
+#     a = a // 2
+ 
+# print(f'Введенное число в двоичной системе равно {b}')
+
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
 # Пример:
 
-# - 45 -> 101101
-# - 3 -> 11
-# - 2 -> 10
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-a = int(input('Введите число: '))
- 
-b = ''
- 
-while a > 0:
-    b = str(a % 2) + b
-    a = a // 2
- 
-print(f'Введенное число в двоичной системе равно {b}')
+k = int(input('Введите число k: '))
+
+def Fibonacci(n):
+    if n in [1, 2]:                       
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+def NegaFibonacci(n):
+    if n == 1:                       
+        return 1
+    elif n == 2:                       
+        return -1
+    else:
+        num1, num2 = 1, -1
+        for i in range(2, n):
+            num1, num2 = num2, num1 - num2
+        return num2
+
+list = [0]
+for e in range(1, k + 1):
+    list.append(Fibonacci(e))
+    list.insert(0, NegaFibonacci(e))
+print(list)
