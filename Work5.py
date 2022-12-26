0598e916d384c5b818edf226fc96e01403708ab8
romanc <roman.zin1993@gmail.com>
2022-12-26 19:27:46 +0300

    comment

--- Work5.py
+++ Work5.py
@@ -9,53 +9,53 @@
 # text = del_words(text)
 # print(text)
 
-# 2 Создайте программу для игры с конфетами человек против человека.
+# # 2 Создайте программу для игры с конфетами человек против человека.
 
-# Условие задачи: На столе лежит 2021 конфета. Играют два игрока, делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# # Условие задачи: На столе лежит 2021 конфета. Играют два игрока, делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 
-# a) Добавьте игру против бота
-# b) Подумайте как наделить бота "интеллектом"
+# # a) Добавьте игру против бота
+# # b) Подумайте как наделить бота "интеллектом"
 
-# вариант человек против бота:
-from random import randint
+# # вариант человек против бота:
+# from random import randint
 
-def input_dat(name):
-    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
-    while x < 1 or x > 28:
-        x = int(input(f"{name}, введите корректное количество конфет, от 1 до 28: "))
-    return x
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет, от 1 до 28: "))
+#     return x
 
 
-def p_print(name, k, counter, value):
-    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
 
-player1 = input("Введите имя первого игрока: ")
-player2 = "Bot"
-value = int(2021) #условие задачи
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
+# player1 = input("Введите имя первого игрока: ")
+# player2 = "Bot"
+# value = int(2021) #условие задачи
+# flag = randint(0,2) # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
 
-counter1 = 0 
-counter2 = 0
+# counter1 = 0 
+# counter2 = 0
 
-while value > 28:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
-    else:
-        k = randint(1,28)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = randint(1,28)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
 
-if flag:
-    print(f"Выиграл {player1}")
-else:
-    print(f"Выиграл {player2}")
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
