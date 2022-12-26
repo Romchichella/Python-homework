40f723fcd1b5cf8a4cd674b9192fac8f4bd9c227
romanc <roman.zin1993@gmail.com>
2022-12-26 19:24:05 +0300

    comment

--- Work5.py
+++ Work5.py
@@ -1,10 +1,11 @@
-# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+# # 1 Напишите программу, удаляющую из текста все слова, содержащие заданную строку. Пример - "Пугать ты галок пугай => заданная строка "пугай" => Пугать ты галок".
 
-text = 'Напишите набвпишите программу, проабв удаляющую из этого абв текста все словабв слова, содерабващие содержащие "а.б.в"'
+# text = 'Пугать ты галок пугай'
 
-def del_words(text):
-    text = list(filter(lambda a: 'абв' not in a, text.split()))
-    return " ".join(text)
+# def del_words(text):
+#     text = list(filter(lambda a: 'пугай' not in a, text.split()))
+#     return " ".join(text)
 
-text = del_words(text)
-print(text)
+# text = del_words(text)
+# print(text)
+
