1388b7524cf6557950491195142d48657b19a05b
romanc <roman.zin1993@gmail.com>
2022-12-29 17:48:16 +0300

    comment

--- Work6.py
+++ Work6.py
@@ -23,26 +23,29 @@
 # trigger_str = input('Ввести искомую строку с маленькой буквы\n')
 # print(show_second_index(input_list, trigger_str))
 
-# 2- Найти произведение пар чисел в списке.
+# 2 Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-# Пример:
 
-# [2, 3, 4, 5, 6] => [12, 15, 16];
-# [2, 3, 5, 6] => [12, 15]
-
-
+from gbfunctions import give_int, random_list
 from typing import List
-from functions import give_int
-from functions import create_random_list
 
 
-def get_products_of_pairs(lst: List[int]) -> List[int]:
-    
-    product_lst = [lst[i] * lst[-1 - i]
-                   for i in range(len(lst) // 2 + len(lst) % 2)]
-    return (product_lst)
+def pair_mult(data_list: List[int]) -> List[int]:
+    '''
+    Gives multiplication of elements on opposite indexes
+    :param data_list: list with elements
+    :return: list with multiplication of elements on opposite indexes
+    '''
+
+    return [data_list[i]*data_list[-1 - i] for i in range(len(data_list)//2 + len(data_list) % 2)]
 
 
-list1 = create_random_list(0, 15)
-print(list1)
-print(get_products_of_pairs(list1))
+dt_lst = random_list(4)
+print(dt_lst)
+res = pair_mult(dt_lst)
+print(res)
+
+dt_lst = random_list(5)
+print(dt_lst)
+res = pair_mult(dt_lst)
+print(res)
