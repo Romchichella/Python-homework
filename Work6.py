44b962ca3af1525b9f7b41b4e0d25e33711ebba3
romanc <roman.zin1993@gmail.com>
2022-12-29 17:12:53 +0300

    comment

--- Work6.py
+++ Work6.py
@@ -22,3 +22,29 @@
 # input_list = list((input('Ввести элементы списка через пробел\n')).split(' '))
 # trigger_str = input('Ввести искомую строку с маленькой буквы\n')
 # print(show_second_index(input_list, trigger_str))
+
+# 2- Найти произведение пар чисел в списке.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+
+# [2, 3, 4, 5, 6] => [12, 15, 16];
+# [2, 3, 5, 6] => [12, 15]
+
+
+from typing import List
+from functions import give_int
+from functions import create_random_list
+
+
+def get_products_of_pairs(lst: List[int]) -> List[int]:
+    '''
+    Возвращает список произведений пар чисел в исходном списке
+    '''
+    product_lst = [lst[i] * lst[-1 - i]
+                   for i in range(len(lst) // 2 + len(lst) % 2)]
+    return (product_lst)
+
+
+list1 = create_random_list(0, 15)
+print(list1)
+print(get_products_of_pairs(list1))
