d2a1caee0e5f2def645bf249929a0e702170feab
romanc <roman.zin1993@gmail.com>
2022-12-29 17:50:46 +0300

    comment

--- Work6.py
+++ Work6.py
@@ -23,29 +23,37 @@
 # trigger_str = input('Ввести искомую строку с маленькой буквы\n')
 # print(show_second_index(input_list, trigger_str))
 
-# 2 Напишите программу, которая найдёт произведение пар чисел списка.
-# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# # 2 Напишите программу, которая найдёт произведение пар чисел списка.
+# # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-from gbfunctions import give_int, random_list
-from typing import List
-
-
-def pair_mult(data_list: List[int]) -> List[int]:
-    '''
-    Gives multiplication of elements on opposite indexes
-    :param data_list: list with elements
-    :return: list with multiplication of elements on opposite indexes
-    '''
-
-    return [data_list[i]*data_list[-1 - i] for i in range(len(data_list)//2 + len(data_list) % 2)]
+# from gbfunctions import give_int, random_list
+# from typing import List
 
 
-dt_lst = random_list(4)
-print(dt_lst)
-res = pair_mult(dt_lst)
-print(res)
+# def pair_mult(data_list: List[int]) -> List[int]:
+#     '''
+#     Gives multiplication of elements on opposite indexes
+#     :param data_list: list with elements
+#     :return: list with multiplication of elements on opposite indexes
+#     '''
 
-dt_lst = random_list(5)
-print(dt_lst)
-res = pair_mult(dt_lst)
-print(res)
+#     return [data_list[i]*data_list[-1 - i] for i in range(len(data_list)//2 + len(data_list) % 2)]
+
+
+# dt_lst = random_list(4)
+# print(dt_lst)
+# res = pair_mult(dt_lst)
+# print(res)
+
+# dt_lst = random_list(5)
+# print(dt_lst)
+# res = pair_mult(dt_lst)
+# print(res)
+
+# 3-Сформировать список из N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+from gbfunctions import give_int
+
+result = [(-3)**i for i in range(give_int('Type amount of numbers: '))]
+print(result)
