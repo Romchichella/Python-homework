bd90e5297626046ae6182f930b2cadf03b6fc4f9
romanc <roman.zin1993@gmail.com>
2022-12-29 17:55:19 +0300

    comment

--- Work6.py
+++ Work6.py
@@ -26,7 +26,7 @@
 # # 2 Напишите программу, которая найдёт произведение пар чисел списка.
 # # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-# from gbfunctions import give_int, random_list
+# from functions import give_int, random_list
 # from typing import List
 
 
@@ -53,7 +53,7 @@
 # 3 Сформировать список из N членов последовательности.
 # # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-# from gbfunctions import give_int
+# from functions import give_int
 
 # result = [(-3)**i for i in range(give_int('Type amount of numbers: '))]
 # print(result)
