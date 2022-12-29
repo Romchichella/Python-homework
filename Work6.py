c2ad040e81ca8b8b1bc4284411ab84f1b4414c2b
romanc <roman.zin1993@gmail.com>
2022-12-29 18:21:24 +0300

    comment

--- Work6.py
+++ Work6.py
@@ -82,40 +82,40 @@
 # а второй - сам элемент, при условии, что они не совпадают.
 # [1,1,1,1] -> [(0,1),(1,1),(2,1),(3,1)] -> [(0,1),(2,1),(3,1)]
 
-# 6 - Из списка выше оставьте только те пары, где сумма кортежа кратна 5
-# Пример
-# [(10,25),(3,4),(4,1)] => [(10,25),(4,1)]
+# # 6 - Из списка выше оставьте только те пары, где сумма кортежа кратна 5
+# # Пример
+# # [(10,25),(3,4),(4,1)] => [(10,25),(4,1)]
 
-from functions import create_random_list
-from typing import List
+# from functions import create_random_list
+# from typing import List
 
 
-def check_coincidence(i) -> bool:
+# def check_coincidence(i) -> bool:
     
-    t1, t2 = i
-    if t1 == t2:
-        return False
-    else:
-        return True
+#     t1, t2 = i
+#     if t1 == t2:
+#         return False
+#     else:
+#         return True
 
 
-def check_multiple_5(i) -> bool:
+# def check_multiple_5(i) -> bool:
    
-    t1, t2 = i
-    if (t1 + t2) % 5:
-        return False
-    else:
-        return True
+#     t1, t2 = i
+#     if (t1 + t2) % 5:
+#         return False
+#     else:
+#         return True
 
 
-lst = create_random_list(1, 10)
-print(lst)
+# lst = create_random_list(1, 10)
+# print(lst)
 
-tuples_lst = list(enumerate(lst))
-print(tuples_lst)
+# tuples_lst = list(enumerate(lst))
+# print(tuples_lst)
 
-result1 = list(filter(check_coincidence, tuples_lst))
-print(result1)
+# result1 = list(filter(check_coincidence, tuples_lst))
+# print(result1)
 
-result2 = list(filter(check_multiple_5, result1))
-print(result2)
+# result2 = list(filter(check_multiple_5, result1))
+# print(result2)
